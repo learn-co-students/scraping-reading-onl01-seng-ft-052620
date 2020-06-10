1f662f65e0aa29ec2ba38287238370f0e19c9555
dwandrew <danwandrew@gmail.com>
2020-06-10 12:29:51 +0100

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,14 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+# html = open("https://flatironschool.com/")
+def scraper
+doc =Nokogiri::HTML(open("https://flatironschool.com/"))
+course_doc =Nokogiri::HTML(open("https://flatironschool.com/our-courses/"))
+puts doc.css(".headline-26OIBN").text
+course_titles = course_doc.css(".title-oE5vT4")
+courses = course_titles.map{|course| course.text}
+puts courses
+end
+
+scraper
